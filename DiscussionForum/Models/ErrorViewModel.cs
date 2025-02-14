7e70445889e3cae1d2fa16f63f31660cbb3aef08
Omar Ahmed <W0475798@campus.nscc.ca>
2025-02-14 18:22:49 -0400

    Add project files.

---
+++ DiscussionForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DiscussionForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
